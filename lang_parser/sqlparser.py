ad959e83119f108f83718df564ad06a2ce93e69f
Spandan Bemby <spandanb@users.noreply.github.com>
2021-10-28 21:12:49 -0700

    break frontend into smaller modules

---
+++ lang_parser/sqlparser.py
@@ -0,0 +1,238 @@
+from tokens import TokenType, KEYWORDS, Token
+from symbols import *
+
+
+class Parser:
+    """
+    sql parser
+
+    grammar :
+        program -> stmnt* EOF
+
+        stmnt   -> select_expr
+                | create_table_stmnt
+                | insert_stmnt
+                | update_stmnt
+                | delete_stmnt
+
+        select_expr  -> "select" selectable "from" from_item "where" where_clause
+        selectable    -> (column_name ",")* (column_name)
+        column_name   -> IDENTIFIER
+        from_location -> IDENTIFIER
+        where_clause  -> (and_clause "or")* (and_clause)
+        and_clause    -> (predicate "and")* (predicate)
+        predicate     -> term ( ( ">" | ">=" | "<" | "<=" | "<>" | "=" ) term ) ;
+
+        create_table_stmnt -> "create" "table" table_name "(" column_def_list ")"
+        column_def_list -> (column_def ",")* column_def
+        column_def -> column_name datatype ("primary key")? ("not null")?
+        table_name -> IDENTIFIER
+
+        -- term should also handle other literals
+        term          -> NUMBER | STRING | IDENTIFIER
+    """
+    def __init__(self, tokens: List):
+        self.tokens = tokens
+        self.current = 0  # pointer to current token
+        self.errors = []
+
+    def advance(self) -> Token:
+        """
+        consumes current token(i.e. increments
+        current pointer) and returns it
+        """
+        if self.is_at_end() is False:
+            self.current += 1
+        return self.previous()
+
+    def consume(self, token_type: TokenType, message: str) -> Token:
+        """
+        check whether next token matches expectation; otherwise
+        raises exception
+        """
+        if self.check(token_type):
+            return self.advance()
+        self.errors.append((self.peek(), message))
+        raise ParseError(self.peek(), message)
+
+    def peek(self) -> Token:
+        return self.tokens[self.current]
+
+    def previous(self) -> Token:
+        return self.tokens[self.current - 1]
+
+    def is_at_end(self) -> bool:
+        return self.peek().token_type == TokenType.EOF
+
+    def match(self, *types: TokenType) -> bool:
+        """
+        if any of `types` match the current type consume and return True
+        """
+        for token_type in types:
+            if self.check(token_type):
+                self.advance()
+                return True
+        return False
+
+    def match_symbol(self, symbol_type: Type[Symbol]) -> Union[Tuple[bool, Any]]:
+        """
+        Analog to `match` that `matches` on symbol instead of tokens.
+
+
+        :return: tuple: (is_match, matched_symbol)
+        """
+        # store token ptr position, so we can reset
+        # otherwise, on non-match, some tokens would still be consumed.
+        old_current = self.current
+
+        # determine method that will attempt to parse the symbol
+        symbol_typename = symbol_type.__name__.__str__()
+        handler_name = pascal_to_snake_case(symbol_typename)
+        handler = getattr(self, handler_name)
+        try:
+            symbol = handler()
+            return True, symbol
+        except ParseError as e:
+            self.current = old_current
+            return False, None
+
+    def check(self, token_type: TokenType):
+        """
+        return True if `token_type` matches current token
+        """
+        if self.is_at_end():
+            return False
+        return self.peek().token_type == token_type
+
+    def error(self, token, message: str):
+        """
+        report error and return error sentinel object
+        """
+        self.errors.append((token, message))
+
+    # section: rule handlers
+
+    def program(self) -> Symbol:
+        program = []
+        while not self.is_at_end():
+            if self.peek().token_type == TokenType.SELECT:
+                # select statement
+                program.append(self.select_expr())
+            elif self.peek().token_type == TokenType.CREATE:
+                # create
+                program.append(self.create_table_stmnt())
+        return Program(program)
+
+    def create_table_stmnt(self) -> CreateTableStmnt:
+        self.consume(TokenType.CREATE, "Expected keyword [CREATE]")
+        self.consume(TokenType.TABLE, "Expected keyword [TABLE]")
+        table_name = self.table_name()
+        self.consume(TokenType.LEFT_PAREN, "Expected [(]")  # '('
+        column_def_list = self.column_def_list()
+        self.consume(TokenType.RIGHT_PAREN, "Expected [)]")
+        CreateTableStmnt(table_name=table_name, column_def_list=column_def_list)
+
+    def column_def_list(self) -> List[ColumnDef]:
+        column_defs = []
+        while True:
+            is_match, matched_symbol = self.match_symbol(ColumnDef)
+            if not is_match:
+                break
+            column_defs.append(matched_symbol)
+
+        return column_defs
+
+    def table_name(self) -> Token:
+        return self.consume(TokenType.IDENTIFIER, "Expected identifier for table name")
+
+    def column_def(self) -> ColumnDef:
+        """
+        column_def -> column_name datatype ("primary key")? ("not null")?
+        :return:
+        """
+        col_name = self.column_name()
+        # todo: there should be a validation on datatype
+        datatype_token = self.consume(TokenType.IDENTIFIER, "Expected a datatype identifier")
+        return ColumnDef(col_name, datatype_token)
+
+    def select_expr(self) -> SelectExpr:
+        # select and from are required
+        self.consume(TokenType.SELECT, "Expected keyword [SELECT]")
+        selectable = self.selectable()
+        # todo: make from clause optional
+        self.consume(TokenType.FROM, "Expected keyword [FROM]")
+        from_location = self.from_location()
+        # where clause is optional
+        where_clause = None
+        if self.match(TokenType.WHERE):
+            where_clause = self.where_clause()
+
+        return SelectExpr(selectable=selectable, from_location=from_location, where_clause=where_clause)
+
+    def selectable(self):
+        # there must be at least one column
+        items = [self.column_name()]
+        while self.match(TokenType.COMMA):
+            # keep looping until we see commas
+            items.append(self.column_name())
+        return Selectable(items)
+
+    def column_name(self) -> Token:
+        return self.consume(TokenType.IDENTIFIER, "expected identifier for column name")
+
+    def from_location(self) -> Token:
+        return self.consume(TokenType.IDENTIFIER, "expected identifier for from location")
+
+    def where_clause(self):
+        """
+        where_clause  -> (and_clause "or")* (and_clause)
+        :return:
+        """
+        clauses = [self.and_clause()]
+        while self.match(TokenType.OR):
+            clauses.append(self.and_clause())
+        return WhereClause(clauses)
+
+    def and_clause(self) -> AndClause:
+        """
+        and_clause -> (predicate "and")* (predicate)
+        """
+        predicates = [self.predicate()]
+        while self.match(TokenType.AND):
+            predicates.append(self.predicate())
+        return AndClause(predicates=predicates)
+
+    def predicate(self) -> Predicate:
+        """
+        predicate     -> term ( ( ">" | ">=" | "<" | "<=" | "<>" | "=" ) term ) ;
+        :return:
+        """
+        first = self.term()
+        operator = None
+        if self.match(TokenType.LESS, TokenType.LESS_EQUAL, TokenType.GREATER, TokenType.GREATER_EQUAL,
+                      TokenType.NOT_EQUAL, TokenType.EQUAL):
+            operator = self.previous()
+        second = self.term()
+        return Predicate(first=first, op=operator, second=second)
+
+    def term(self):
+        """
+        -- term should also handle other literals
+        term          -> NUMBER | STRING | IDENTIFIER
+
+        :return:
+        """
+        if self.match(TokenType.NUMBER, TokenType.STRING, TokenType.IDENTIFIER):
+            return Term(value=self.previous())
+
+        raise ParseError("expected a valid term")
+
+    def parse(self) -> List:
+        """
+        parse tokens and return a list of statements
+        :return:
+        """
+        statements = []
+        while self.is_at_end() is False:
+            statements.append(self.program())
+        return statements
---
+++ lang_parser/tokens.py
@@ -0,0 +1,103 @@
+"""
+Contains types related to tokens
+"""
+from enum import Enum, auto
+from typing import Any, List, Type, Tuple, Union
+
+
+class TokenType(Enum):
+    """
+    types of tokens
+    """
+    # single-char tokens
+    STAR = auto()  # '*'
+    LEFT_PAREN = auto()  # '(
+    RIGHT_PAREN = auto()  # ')'
+    LEFT_BRACKET = auto()  # '['
+    RIGHT_BRACKET = auto()  # ']'
+    DOT = auto()  # '.'
+    EQUAL = auto()  # =
+    LESS = auto()  # <
+    GREATER = auto()  # >
+    COMMA = auto()  # ,
+
+    # 2-char tokens
+    LESS_EQUAL = auto()  # <=
+    GREATER_EQUAL = auto()  # >=
+    NOT_EQUAL = auto()  # represents both: <>, !=
+
+    # misc
+    EOF = auto()
+    IDENTIFIER = auto()
+    NUMBER = auto()
+    STRING = auto()
+
+    # keywords
+    FROM = auto()
+    OR = auto()
+    WHERE = auto()
+    ON = auto()
+    CREATE = auto()
+    AND = auto()
+    DELETE = auto()
+    INSERT = auto()
+    JOIN = auto()
+    CASE = auto()
+    HAVING = auto()
+    GROUP = auto()
+    ORDER = auto()
+    UPDATE = auto()
+    BY = auto()
+    SELECT = auto()
+    NULL = auto()
+
+
+KEYWORDS = {
+    'select',
+    'from',
+    'where',
+    'join',
+    'on',
+    'group',
+    'order',
+    'by',
+    'having',
+    'case',
+    'or',
+    'and',
+    'null',
+    'create',
+    'delete',
+    'update',
+    'insert',
+
+    #  datatypes
+    'integer',
+    'real',  # floating point number
+    'text',  # variable length text
+
+}
+
+
+class Token:
+    """
+    Represents a token of the source
+    """
+    def __init__(self, token_type: TokenType, lexeme: str, literal: Any, line: int):
+        self.token_type = token_type
+        self.lexeme = lexeme
+        self.literal = literal
+        self.line = line
+
+    def __str__(self) -> str:
+        # when object is printed
+        body = f'{self.token_type}'
+        tail = self.literal or self.lexeme
+        if tail:
+            body = f'{body}[{tail}]'
+        return body
+
+    def __repr__(self) -> str:
+        # appears in collections
+        return f'Token({self.__str__()})'
+
