151913f9c076a665b9a6948641870071d726347a
Spandan Bemby <spbemby@microsoft.com>
2021-10-28 21:11:47 -0700

    begin refactor

---
+++ lang_parser/sqlhandler.py
@@ -0,0 +1,36 @@
+from __future__ import annotations
+
+from tokenizer import Tokenizer
+from sqlparser import Parser
+
+
+def sql_handler(source: str):
+    """
+    tokenize, parse input and return representation of source
+
+    :param source: input to parse
+    :return:
+    """
+    # tokenize
+    tokenizer = Tokenizer(source, None)
+    tokens = tokenizer.scan_tokens()
+    print(f'scanned tokens: {tokens}')
+    if tokenizer.errors:
+        print("Error: scanner failed with following errors:")
+        print(tokenizer.errors)
+        return
+
+    # parse the tokens
+    parser = Parser(tokens)
+    statements = parser.parse()
+    print(f'parsed statements: {statements}')
+    if parser.errors:
+        print("Error: parser failed with following errors:")
+        print(parser.errors)
+        return
+
+
+if __name__ == "__main__":
+    cmdtext = "select colA from foo where colA <> 4.2"
+    # cmdtext = "select colA, colFOO from foo"
+    sql_handler(cmdtext)
