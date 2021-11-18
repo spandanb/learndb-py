0787b0400c06e127d24742fe2d009f080db52fed
Spandan Bemby <spandanb@users.noreply.github.com>
2021-11-17 22:24:41 -0800

    add stress test stub

---
+++ stress.py
@@ -0,0 +1,10 @@
+"""
+This is a stub for "stress" tests, which will perform
+a large number of random operations or perform them
+for a fixed amount of time.
+
+These should compliment, static unit tests, in that they
+should run non-deterministically, and thus expose issues
+that unit-tests can't catch.
+"""
+
