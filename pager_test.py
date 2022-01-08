be6fa8c33bfe2fc779ba672c21308ba1b83ac67f
Spandan Bemby <spandan.777@gmail.com>
2022-01-07 18:13:23 -0800

    update pager to persist pages on close

---
+++ pager_test.py
@@ -0,0 +1,32 @@
+"""
+Get a page, return a page. close pager.
+"""
+
+from pager import Pager
+from constants import TEST_DB_FILE
+
+
+def test_free_pages_persisted():
+    """
+    Test that returned pages are persisted
+    and re-served after pager is closed and reopened.
+    :return:
+    """
+    pager = Pager(TEST_DB_FILE)
+    first = pager.get_unused_page_num()
+    second = pager.get_unused_page_num()
+    third = pager.get_unused_page_num()
+
+    # don't return third - to avoid file truncation
+    pager.return_page(first)
+    pager.return_page(second)
+
+    # close pager and see if the returned pages
+    # are given when an unused page is requested
+    pager.close()
+
+    pager = Pager(TEST_DB_FILE)
+    new_page = pager.get_unused_page_num()
+    assert new_page == first or new_page == second
+    new_page = pager.get_unused_page_num()
+    assert new_page == first or new_page == second
