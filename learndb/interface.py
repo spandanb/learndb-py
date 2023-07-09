18931ef82e891f4cd3b0a0368b71af871c3193d2
Spandan Bemby <spandan.777@gmail.com>
2023-07-09 00:06:18 -0400

    move btree devloop into stress test module

--- learndb/interface.py
+++ learndb/interface.py
@@ -6,22 +6,17 @@ the DBMS functionality that is learndb.
 """
 import os
 import os.path
-import math  # for testing
 import sys
-import random
 import logging
-import itertools  # for testing
 
-from typing import Union, List, Any
-from dataclasses import dataclass
-from enum import Enum, auto
-from random import randint, shuffle  # for testing
+from typing import List
 
 from .constants import DB_FILE, USAGE, EXIT_SUCCESS, EXIT_FAILURE
 from .lang_parser.sqlhandler import SqlFrontEnd
 from .lang_parser.symbols import Program
 from .dataexchange import Response, MetaCommandResult
 from .pipe import Pipe
+from .stress import run_add_del_stress_suite
 from .virtual_machine import VirtualMachine, VMConfig
 
 
@@ -96,7 +91,7 @@ class LearnDB:
     def nuke_dbfile(self):
         """
         remove db file.
-        This effectively removes
+        This effectively restarts the instance into a clean state.
         :return:
         """
         if os.path.exists(self.db_filepath):
@@ -273,376 +268,42 @@ def run_file(input_filepath: str, db_filepath: str = DB_FILE) -> Response:
     db.close()
 
 
-def devloop_add_del():
+def run_stress(db_filepath: str = DB_FILE):
     """
-    this works through the entire intialize process
-    :return:
+    Run stress test
     """
+    db = LearnDB(db_filepath)
+    run_add_del_stress_suite(db)
 
-    test_cases = [
-        [1, 2, 3, 4],
-        [64, 5, 13, 82],
-        [82, 13, 5, 2, 0],
-        [10, 20, 30, 40, 50, 60, 70],
-        [72, 79, 96, 38, 47],
-        [432, 507, 311, 35, 246, 950, 956, 929, 769, 744, 994, 438],
-        [159, 597, 520, 189, 822, 725, 504, 397, 218, 134, 516],
-        [159, 597, 520, 189, 822, 725, 504, 397],
-        [960, 267, 947, 400, 795, 327, 464, 884, 667, 870, 92],
-        [793, 651, 165, 282, 177, 439, 593],
-        [229, 653, 248, 298, 801, 947, 63, 619, 475, 422, 856, 57, 38],
-        [103, 394, 484, 380, 834, 677, 604, 611, 952, 71, 568, 291, 433, 305],
-        [114, 464, 55, 450, 729, 646, 95, 649, 59, 412, 546, 340, 667, 274, 477, 363, 333, 897, 772, 508, 182,
-        305, 428,
-        180, 22],
-        [15, 382, 653, 668, 139, 70, 828, 17, 891, 121, 175, 642, 491, 281, 920],
-        [967, 163, 791, 938, 939, 196, 104, 465, 886, 355, 58, 251, 928, 758, 535, 737, 357, 125, 171, 838,
-        572, 745,
-        999, 417, 393, 458, 292, 904, 158, 286, 900, 859, 668, 183],
-        [726, 361, 583, 121, 908, 789, 842, 67, 871, 461, 522, 394, 225, 637, 792, 393, 656, 748, 39, 696],
-        [54, 142, 440, 783, 619, 273, 95, 961, 692, 369, 447, 825, 555, 908, 483, 356, 40, 110, 519, 599],
-        [413, 748, 452, 666, 956, 926, 94, 813, 245, 237, 264, 709, 706, 872, 535, 214, 561, 882, 646]
-    ]
-
-    # basic
-    #insert_keys = [967, 163, 791, 938, 939, 196, 104, 465, 886, 355, 58, 251, 928, 758, 535, 737, 357, 125, 171, 838, 572, 745, 999, 417, 393, 458, 292, 904, 158, 286, 900, 859, 668, 183]
-    #del_keys =    [967, 163, 791, 938, 939, 196, 104, 465, 886, 355, 58, 251, 928, 758, 535, 737, 357, 125, 171, 838, 572, 745, 999, 417, 393, 458, 292, 904, 158, 286, 859, 668, 183, 900]
-    #inner_devloop(insert_keys, del_keys)
-
-    # stress
-    for test_case in test_cases:
-
-        insert_keys = test_case
-        # del_keys = test_case[:]
-
-        # there is a large number of perms ~O(n!)
-        # and they are generated in a predictable order
-        # we'll skip based on fixed step- later, this too should be randomized
-        num_perms = 1
-        total_perms = math.factorial(len(insert_keys))
-        del_perms = []
-
-        step_size = min(total_perms//num_perms, 10)
-        # iterator over permutations
-        perm_iter = itertools.permutations(insert_keys)
-
-        while len(del_perms) < num_perms:
-            for _ in range(step_size-1):
-                # skip n-1 deletes
-                next(perm_iter)
-            del_perms.append(next(perm_iter))
-
-        for del_keys in del_perms:
-            try:
-                inner_devloop(insert_keys, del_keys)
-            except Exception as e:
-                logging.error(f'Inner devloop failed on: {insert_keys} {del_keys} with {e}')
-                raise
-
-
-def inner_devloop(insert_keys, del_keys):
-    """
-    perform some ops/validations
-    
-    :param db: 
-    :param insert_keys: 
-    :param del_keys: 
-    :return: 
-    """
-    
-    db = LearnDB(DB_FILE)
-    db.nuke_dbfile()
-
-    print(f'running test case: {insert_keys} {del_keys}')
-
-    # random.shuffle(del_keys)
-    cmd = "create table foo ( colA integer primary key, colB text)"
-    logging.info(f"handling [{cmd}]")
-    resp = db.handle_input(cmd)
-
-    # insert
-    for key in insert_keys:
-        cmd = f"insert into foo (colA, colB) values ({key}, 'hellew words foo')"
-        logging.info(f"handling [{cmd}]")
-        resp = db.handle_input(cmd)
-
-    logging.debug("printing tree.......................")
-    db.state_manager.print_tree("foo")
-
-    # delete and validate
-    for idx, key in enumerate(del_keys):
-        # cmd = f"delete from foo where colA = {key} AND colB = 'foo'"
-        cmd = f"delete from foo where colA = {key}"
-        logging.info(f"handling [{cmd}]")
-        resp = db.handle_input(cmd)
-        if not resp.success:
-            print(f"cmd {cmd} failed with {resp.status} {resp.error_message}")
-            return EXIT_SUCCESS
-
-        resp = db.handle_input("select cola, colb from foo")
-        assert resp.success
-
-        # output pipe
-        pipe = db.get_pipe()
-
-        result_keys = []
-        # print anything in the output buffer
-        logging.debug(f'pipe has msgs: {pipe.has_msgs()}')
-        while pipe.has_msgs():
-            record = pipe.read()
-            key = record.get("cola")
-            print(f'pipe read: {record}')
-            result_keys.append(key)
-
-        # assert result_keys == [k for k in sorted(keys)], f"result {result_keys} doesn't not match {[k for k in sorted(keys)]}"
-
-        logging.debug("printing tree.......................")
-        db.state_manager.print_tree("foo")
-        # ensure tree is valid
-        db.state_manager.validate_tree("foo")
-
-        # check if all keys we expect are there in result
-        expected = [key for key in sorted(del_keys[idx+1:])]
-        actual = [key for key in sorted(set(result_keys))]
-        assert actual == expected, f"expected: {expected}; received {actual}"
-
-        print('*'*100)
-
-    db.close()
-
-
-def devloop_join():
-
-    db = LearnDB(DB_FILE)
-    db.nuke_dbfile()
-
-    # create table
-    db.handle_input("create table foo ( cola integer primary key, colb integer, colc integer)")
-    db.handle_input("create table bar ( colx integer primary key, coly integer, colz integer)")
-    # insert into table
-    db.handle_input("insert into foo (cola, colb, colc) values (1, 2, 3)")
-    db.handle_input("insert into foo (cola, colb, colc) values (2, 4, 6)")
-    db.handle_input("insert into foo (cola, colb, colc) values (3, 10, 8)")
-    db.handle_input("insert into bar (colx, coly, colz) values (101, 10, 80)")
-    db.handle_input("insert into bar (colx, coly, colz) values (102, 4, 90)")
-    # select
-    db.handle_input("select b.colx, b.coly, b.colz from foo f join bar b on f.colb = b.coly")
-
-    keys = []
-
-    assert db.get_pipe().has_msgs()
-    while db.get_pipe().has_msgs():
-        record = db.get_pipe().read()
-        # NOTE: records access API is different for Record and MultiRecord
-        # keys.append(record.get("b", "colx"))
-        rdict = record.to_dict()
-        keys.append(rdict)
-    # TODO: this is not working
-    expected = [101, 102]
-    assert keys == expected, f"expected {expected}; received {keys}"
-
-
-def devloop_old():
-    # TODO: before cleaning/nuking these, ensure that the debugging capabilities/flows
-    # these expose, are kept somewhere, perhaps a standalone debug driver?
-    # somewhere; or perhaps just kept in this file
-    db = LearnDB(DB_FILE)
-    db.nuke_dbfile()
-
-    db.handle_input("create table foo ( cola integer primary key, colb integer, colc integer)")
-    db.handle_input("create table bar ( colx integer primary key, coly integer, colz integer)")
-    # db.handle_input("create table car ( colx integer primary key, coly integer, colz integer)")
-    db.handle_input("insert into foo ( cola, colb, colc) values (1, 2, 3)")
-    db.handle_input("insert into foo ( cola, colb, colc) values (2, 4, 6)")
-    db.handle_input("insert into bar ( colx, coly, colz) values (30, 20, 40)")
-    # db.handle_input("select cola, colb from foo where cola = 1 or colc = 2")
-    # db.handle_input("select cola, colb from foo where cola = 1 and colc = 3 or colb = 2")
-    # db.handle_input("select cola, colb from foo where cola = 1 and colc = 3 or colb = 2")
-    # db.handle_input("select cola, colb from foo f inner join bar r on f.cola = r.coly")
-    db.handle_input("select cola, colb from foo f inner join bar r on f.b = r.y inner join car c on c.x = f.b")
-    #db.handle_input("select cola, colb from foo f cross join bar r on f.x = r.y")  # cross join should not have an on-clause
-    #db.handle_input("select cola, colb from foo f left join bar r on f.x = r.y")
-
-    assert db.get_pipe().has_msgs()
-
-    while db.get_pipe().has_msgs():
-        record = db.get_pipe().read()
-        # key = record.get("cola")
-        print(f'pipe read: {record}')
 
 
 def devloop():
+    # todo: nuke me
+    
     db = LearnDB(DB_FILE)  # nuke_db_file=True)
-
-    text = "select cola from foo f cross join june"
-    text = "select cola, colb from foo f left join bar r on fx = ry;"
-    text = "select cola, colb from foo f left join bar b on x = 1 left join car c on y = 2 left join dar d on fx = ry;"
-    text = "select cola, colb from foo f join bar b on x = 1 left join car c on y = 2 left join dar d on fx = ry;"
-    text = "select cola, colb from foo f left join bar b on x = 1;"
-
-
-    text = "create table foo (cola integer primary key, colb text)"
-    text = "select cola from catalog"
-    #text = "select cola from foo"
-    text = "select cola from foo where cola > 5"
-    #resp = db.handle_input(text)
-    #db.close()
-    #return
-
-    texts = [
-        "create table foo ( cola integer primary key, colb integer, colc integer)",
-        "create table bar ( colx integer primary key, coly integer, colz integer)",
-        "create table car ( colx integer primary key, coly integer, colz integer)",
-        # insert into table
-        #"insert into foo (cola, colb, colc) values (1, 2, 3)",
-        #"insert into foo (cola, colb, colc) values (2, 4, 6)",
-        "insert into foo (cola, colb, colc) values (3, 10, 8)",
-        "insert into bar (colx, coly, colz) values (101, 10, 80)",
-        "insert into bar (colx, coly, colz) values (102, 4, 90)",
-        "insert into car (colx, coly, colz) values (101, 10, 89)",
-        #"insert into car (colx, coly, colz) values (102, 5, 91)",
-        # "select b.colx, b.coly, b.colz from foo f",
-        "select b.colx, b.coly, b.colz from foo f join bar b on f.colb = b.coly",
-        "select b.colx, b.coly, b.colz from foo f join bar b on f.colb = b.coly join car c on f.colb = c.coly",
-    ]
-
-    # scoped select
-    texts = [
-        "create table foo ( cola integer primary key, colB integer, colc integer, cold integer)",
-        "insert into foo (cola, colb, colc, cold) values (1, 2, 31, 4)",
-        "insert into foo (cola, colb, colc, cold) values (2, 4, 6, 8)",
-        "insert into foo (cola, colb, colc, cold) values (3, 10, 3, 8)",
-        "select f.cola from foo f where f.colb = 4 AND f.colc = 6 OR f.colc = 3"
-        #"select f.cola from foo f where f.colb = 4"
-    ]
-
-    # group by + having
-    texts = [
-        "create table items ( custid integer primary key, country integer)",
-        "insert into items (custid, country) values (10, 1)",
-        "insert into items (custid, country) values (20, 1)",
-        "insert into items (custid, country) values (100, 2)",
-        "insert into items (custid, country) values (200, 2)",
-        "insert into items (custid, country) values (300, 2)",
-        # "select f.cola from foo f group by f.colb, f.cola",
-        #"select count(custid), country from items group by country",
-        "select count(custid), country from items group by country having count(cust_id) > 1",
-    ]
-
-
-    # functions, algebra
-    texts = [
-        "create table foo (cola integer primary key, colb text)",
-        "insert into foo ( cola, colb) values (42, 'hello melo')",
-        "insert into foo ( cola, colb) values (1, 'helo melo')",
-        #"insert into foo ( cola, colb) values (43, 'lo melossss')",
-        #"insert into foo ( cola, colb) values (4, 'hello melo')",
-        #"insert into foo ( cola, colb) values (4002, 'heloPIESDS')",
-        "insert into foo ( cola, colb) values (99, 'hello bobo')",
-        #"delete from foo where cola > 99",
-        # "delete from foo",
-        #"select cola from foo where cola > 1 and cola < 100 or colb = 'hello'"
-
-        "select square(cola) + cola from foo"
-
-        # TODO: ensure all statements below return correct result
-        #"select square(cola) from foo where cola = 42"  # TODO: why does this not return any results?
-        #"select square(cola) from foo"
-        #"select cola from foo;",
-        #"select cola, colb from foo where cola = 42",
-        #"select square(cola) from foo;",
-        #"select f.cola from foo f"
-    ]
-
-    # inner join
-    texts = [
-        "create table foo ( cola integer primary key, colb integer, colc integer)",
-        "create table bar ( colx integer primary key, coly integer, colz integer)",
-        "insert into foo (cola, colb, colc) values (1, 2, 3)",
-        "insert into foo (cola, colb, colc) values (2, 4, 6)",
-        "insert into foo (cola, colb, colc) values (3, 10, 8)",
-        "insert into bar (colx, coly, colz) values (101, 10, 80)",
-        "insert into bar (colx, coly, colz) values (102, 4, 90)",
-        # select
-        "select b.colx, b.coly, b.colz from foo f join bar b on f.colb = b.coly",
-        #"select cola from bar"
-    ]
-
-    # group by
-    texts = [
-        "create table items ( custid integer primary key, country integer)",
-        "insert into items (custid, country) values (10, 1)",
-        "insert into items (custid, country) values (20, 1)",
-        "insert into items (custid, country) values (100, 2)",
-        "insert into items (custid, country) values (200, 2)",
-        "insert into items (custid, country) values (300, 2)",
-        "select count(custid), square(country + 1) from items group by country",
-        #"select count(custid), square(country) from items group by country",
-        #"select square(country) from items",
-        #"select 1 from items",
-        #"select country from items group by country",
-
-
-        # tests for expr parsing
-        #"select 1 + 2 from foo where x < 3",
-        #"select somefunc(1, 2, 3)",
-        #"select 1",
-        #"select foo from bar"
-
-        #"select custid, country from items",
-        #"select count(*), country from items group by country", # TODO: this fails to parse
-    ]
-
-    # failing test case: test_select_equality
-    texts = [
-        "create table foo ( cola integer primary key, colB integer, colc integer, cold integer)",
-        "insert into foo (cola, colb, colc, cold) values (1, 2, 31, 4)",
-        "insert into foo (cola, colb, colc, cold) values (2, 4, 6, 8)",
-        "insert into foo (cola, colb, colc, cold) values (3, 10, 3, 8)",
-        "insert into foo (cola, colb, colc, cold) values (4, 6, 90, 8)",
-        #"select cola from foo where cola = 1",
-        #"select cola from foo where colb = 4 AND colc = 6",
-        "select cola from foo where colb = 4 AND colc = 6 OR cold = 4",
-        #"select cola from foo where colb = 4 AND colc = 6 OR colc = 3"
-    ]
-
-    # failing test case - test_select_no_condition_mixed_type_schema
-    # 2 issues; insert not working, no error being shown for column and value list being mismatched
-    texts = [
-        "create table foo ( cola integer primary key, colb text)",
-        "insert into foo (cola, colb) values (1, 'car')",
-        "insert into foo (cola, colb) values (2, 'monkey')",
-        "select colx, colb from foo"
-    ]
-
-    texts = [
-        "create table items ( custid integer primary key, country integer)",
-        "insert into items (custid, country) values (10, 1)",
-        "insert into items (custid, country) values (20, 1)",
-        "insert into items (custid, country) values (100, 2)",
-        "insert into items (custid, country) values (200, 2)",
-        "insert into items (custid, country) values (300, 2)",
-        # "select f.cola from foo f group by f.colb, f.cola",
-        "select count(custid), country from items group by country",
-    ]
-
-    texts = [
-        "create table items ( custid integer primary key, country integer)",
-        "insert into items (custid, country) values (10, 1)",
-        "insert into items (custid, country) values (20, 1)",
-        "insert into items (custid, country) values (100, 2)",
-        "insert into items (custid, country) values (200, 2)",
-        "insert into items (custid, country) values (300, 2)",
-        # "select f.cola from foo f group by f.colb, f.cola",
-        "select count(custid), country from items group by country having count(custid) > 1",
-    ]
-
-    #texts = ["select name, avg_weight from fruits where (avg_weight >= 3.6 and avg_weight <= 10.0) or name = 'mango'"]
-    #texts = ["create table fruits (id integer primary key, name text, avg_weight real);"
-    #    "drop table fruits"]
 
     #texts = ["select name, salary from employees order by salary"]
     texts = ["select name, salary from employees order by salary asc, name desc"]
+    texts = [
+        """CREATE TABLE fruits ( 
+        id INTEGER PRIMARY KEY, 
+        name TEXT, 
+        avg_weight INTEGER)
+        """,
+        "insert into fruits (id, name, avg_weight) values (1, 'apple', 200)",
+        "insert into fruits (id, name, avg_weight) values (2, 'orange', 140)",
+        "insert into fruits (id, name, avg_weight) values (3, 'pineapple', 1000)",
+        "insert into fruits (id, name, avg_weight) values (4, 'grape', 5)",
+        "insert into fruits (id, name, avg_weight) values (5, 'pear', 166)",
+        "insert into fruits (id, name, avg_weight) values (6, 'mango', 150)",
+        "insert into fruits (id, name, avg_weight) values (7, 'watermelon', 10000)",
+        "insert into fruits (id, name, avg_weight) values (8, 'banana', 118)",
+        "insert into fruits (id, name, avg_weight) values (9, 'peach', 147)",
+        #"select name, id from fruits order by id limit 5"
+    ]
+    texts = [
+        "select name, avg_weight from fruits order by avg_weight, name desc limit 4"
+    ]
 
     for text in texts:
         logging.info(f"handling. {text}")
@@ -652,6 +313,7 @@ def devloop():
             logging.info("read from pipe: {}".format(db.pipe.read()))
 
     db.close()
+
 
 
 def parse_args_and_start(args: List):
@@ -673,15 +335,13 @@ python run.py file <filepath>
         return
 
     runmode = args[0].lower()
-    if runmode == 'repl':
+    if runmode == "repl":
         repl()
-    elif runmode == 'join_dloop':
-        devloop_join()
-    elif runmode == 'add_del_dloop':
-        devloop_add_del()
-    elif runmode == 'devloop':
+    elif runmode == "stress":
+        run_stress()
+    elif runmode == "devloop":
         devloop()
-    elif runmode == 'file':
+    elif runmode == "file":
         # todo:  and output file
         # if no output file, write to console
         if len(args) < 2:
--- learndb/stress.py
+++ learndb/stress.py
@@ -7,4 +7,142 @@ These should compliment, static unit tests, in that they
 should run non-deterministically, and thus expose issues
 that unit-tests can't catch.
 """
+import logging
+import itertools
+import math
+
+from .constants import EXIT_SUCCESS
+
+
+def run_add_del_stress_test(db, insert_keys, del_keys):
+    """
+    perform some ops/validations
+
+    :param db:
+    :param insert_keys:
+    :param del_keys:
+    :return:
+    """
+
+    db.nuke_dbfile()
+
+    print(f'running test case: {insert_keys} {del_keys}')
+
+    # random.shuffle(del_keys)
+    cmd = "create table foo ( colA integer primary key, colB text)"
+    logging.info(f"handling [{cmd}]")
+    resp = db.handle_input(cmd)
+
+    # insert
+    for key in insert_keys:
+        cmd = f"insert into foo (colA, colB) values ({key}, 'hellew words foo')"
+        logging.info(f"handling [{cmd}]")
+        resp = db.handle_input(cmd)
+
+    logging.debug("printing tree.......................")
+    db.state_manager.print_tree("foo")
+
+    # delete and validate
+    for idx, key in enumerate(del_keys):
+        # cmd = f"delete from foo where colA = {key} AND colB = 'foo'"
+        cmd = f"delete from foo where colA = {key}"
+        logging.info(f"handling [{cmd}]")
+        resp = db.handle_input(cmd)
+        if not resp.success:
+            print(f"cmd {cmd} failed with {resp.status} {resp.error_message}")
+            return EXIT_SUCCESS
+
+        resp = db.handle_input("select cola, colb from foo")
+        assert resp.success
+
+        # output pipe
+        pipe = db.get_pipe()
+
+        result_keys = []
+        # print anything in the output buffer
+        logging.debug(f'pipe has msgs: {pipe.has_msgs()}')
+        while pipe.has_msgs():
+            record = pipe.read()
+            key = record.get("cola")
+            print(f'pipe read: {record}')
+            result_keys.append(key)
+
+        # assert result_keys == [k for k in sorted(keys)], f"result {result_keys} doesn't not match {[k for k in sorted(keys)]}"
+
+        logging.debug("printing tree.......................")
+        db.state_manager.print_tree("foo")
+        # ensure tree is valid
+        db.state_manager.validate_tree("foo")
+
+        # check if all keys we expect are there in result
+        expected = [key for key in sorted(del_keys[idx + 1:])]
+        actual = [key for key in sorted(set(result_keys))]
+        assert actual == expected, f"expected: {expected}; received {actual}"
+
+        print('*' * 100)
+
+    db.close()
+
+
+def run_add_del_stress_suite(learndb):
+    """
+    Perform a large number of add/del operation
+    and validate btree correctness.
+    :return:
+    """
+
+    test_cases = [
+        [1, 2, 3, 4],
+        [64, 5, 13, 82],
+        [82, 13, 5, 2, 0],
+        [10, 20, 30, 40, 50, 60, 70],
+        [72, 79, 96, 38, 47],
+        [432, 507, 311, 35, 246, 950, 956, 929, 769, 744, 994, 438],
+        [159, 597, 520, 189, 822, 725, 504, 397, 218, 134, 516],
+        [159, 597, 520, 189, 822, 725, 504, 397],
+        [960, 267, 947, 400, 795, 327, 464, 884, 667, 870, 92],
+        [793, 651, 165, 282, 177, 439, 593],
+        [229, 653, 248, 298, 801, 947, 63, 619, 475, 422, 856, 57, 38],
+        [103, 394, 484, 380, 834, 677, 604, 611, 952, 71, 568, 291, 433, 305],
+        [114, 464, 55, 450, 729, 646, 95, 649, 59, 412, 546, 340, 667, 274, 477, 363, 333, 897, 772, 508, 182,
+        305, 428,
+        180, 22],
+        [15, 382, 653, 668, 139, 70, 828, 17, 891, 121, 175, 642, 491, 281, 920],
+        [967, 163, 791, 938, 939, 196, 104, 465, 886, 355, 58, 251, 928, 758, 535, 737, 357, 125, 171, 838,
+        572, 745,
+        999, 417, 393, 458, 292, 904, 158, 286, 900, 859, 668, 183],
+        [726, 361, 583, 121, 908, 789, 842, 67, 871, 461, 522, 394, 225, 637, 792, 393, 656, 748, 39, 696],
+        [54, 142, 440, 783, 619, 273, 95, 961, 692, 369, 447, 825, 555, 908, 483, 356, 40, 110, 519, 599],
+        [413, 748, 452, 666, 956, 926, 94, 813, 245, 237, 264, 709, 706, 872, 535, 214, 561, 882, 646]
+    ]
+
+    # stress
+    for test_case in test_cases:
+
+        insert_keys = test_case
+        # del_keys = test_case[:]
+
+        # there is a large number of perms ~O(n!)
+        # and they are generated in a predictable order
+        # we'll skip based on fixed step- later, this too should be randomized
+        num_perms = 1
+        total_perms = math.factorial(len(insert_keys))
+        del_perms = []
+
+        step_size = min(total_perms//num_perms, 10)
+        # iterator over permutations
+        perm_iter = itertools.permutations(insert_keys)
+
+        while len(del_perms) < num_perms:
+            for _ in range(step_size-1):
+                # skip n-1 deletes
+                next(perm_iter)
+            del_perms.append(next(perm_iter))
+
+        for del_keys in del_perms:
+            try:
+                run_add_del_stress_test(learndb, insert_keys, del_keys)
+            except Exception as e:
+                logging.error(f'Inner devloop failed on: {insert_keys} {del_keys} with {e}')
+                raise
 
